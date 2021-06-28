bc006c665c8cea09fbe52062a790b265c76b6a83
Gabriel Petkewich <gabe.petkewich@gmail.com>
2021-06-28 02:54:15 +0000

    rest of project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Daredevil' },
+    { id: 12, name: 'Superman' },
+    { id: 13, name: 'Thor' },
+    { id: 14, name: 'Starlight' },
+    { id: 15, name: 'The Punisher' },
+    { id: 16, name: 'Legolas' },
+    { id: 17, name: 'Sharkboy' },
+    { id: 18, name: 'Lavagirl' },
+    { id: 19, name: 'Bible Man' },
+    { id: 20, name: 'Minecraft Steve' }
+];
